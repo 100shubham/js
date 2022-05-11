36bce24de0de3203e46940dc9933d85b16376734
100shubham <shubh.brajwasi@gmail.com>
2022-05-11 17:25:08 +0530

    user login

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,7 +1,12 @@
 import React, {Fragment, useState} from 'react'
-import { Link } from 'react-router-dom';
+import { Link} from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { login } from '../../actions/auth';
+import { Navigate } from 'react-router-dom';
 
-const Login = () => {
+
+const Login = ({ login, isAuthenticated }) => {
   const [formData, setFormData] = useState({
     email: '',
     password: ''
@@ -13,9 +18,12 @@ const Login = () => {
 
   const onSubmit = async e => {
       e.preventDefault();
-      console.log('SUCCESS');
+      login(email, password);
     }
   
+  if(isAuthenticated) {
+    return <Navigate replace to ="/dashboard" />
+  }
 
   return (
     <Fragment>
@@ -52,4 +60,13 @@ const Login = () => {
   )
 }
 
-export default Login;
+Login.propTypes = {
+  login: PropTypes.func.isRequired,
+  isAuthenticated: PropTypes.bool
+}
+
+const mapStateToProps = state => ({
+  isAuthenticated: state.auth.isAuthenticated
+})
+
+export default connect(mapStateToProps, {login})(Login);
